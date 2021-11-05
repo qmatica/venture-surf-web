294b12028d0408fc4439ce02e3ce203d56e5346a
taskway <aleksey.khramov93@gmail.com>
2021-11-05 20:34:18 +0300

    синхронизируй названия заголовков с IOS (industies/stages) - они опять другие.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,18 +2,16 @@ import { stagesType } from './types'
 
 export const stages: stagesType = {
   investor: {
+    1: 'First check',
+    2: 'Angel rounds',
+    3: 'Seed rounds',
+    4: 'Series A+'
+  },
+  founder: {
     1: 'Pre-funding',
     2: 'Angel-backed',
     3: 'Venture-backed',
-    4: 'Serias A',
-    5: 'Post Serias A'
-  },
-  founder: {
-    1: 'First Check',
-    2: 'Angel rounds',
-    3: 'Seed rounds',
-    4: 'Series A',
-    5: 'Series A+'
+    4: 'Post Series A'
   }
 }
 
