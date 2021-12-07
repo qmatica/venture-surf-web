e03d0d028116b69dcf198308aa33303ac2c17fdd
taskway <aleksey.khramov93@gmail.com>
2021-12-07 20:53:56 +0300

    Add favicon VS

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -2,13 +2,14 @@ import React, { FC } from 'react'
 import logoHeader from 'common/images/logoHeader.jpg'
 import { NavBar } from 'features/NavBar'
 import { NavLink } from 'react-router-dom'
+import { VSIcon } from 'common/icons'
 import styles from './styles.module.sass'
 
 export const Header: FC = () => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
       <NavLink to="/surf">
-        <img style={{ marginLeft: '-7px' }} src={logoHeader} alt="Venture Surf" draggable={false} />
+        <VSIcon />
       </NavLink>
       <NavBar />
     </div>
