e435e576460714279d86dbb8a0dc4c31c9861cf6
Nonna Musayelyan <nonna_m@instigatemobile.com>
2022-03-16 10:29:26 +0400

    Save deviceId if missing

--- src/features/Auth/components/OnboardingSteps/LinkedInStep/index.tsx
+++ src/features/Auth/components/OnboardingSteps/LinkedInStep/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { profileAPI } from 'api'
+import { v4 as uuidv4 } from 'uuid'
 import { OnboardingUserType } from 'features/Profile/types'
 import { LinkedInIconSm, LinkedInAndVS } from 'common/icons'
 import { linkedInAuthUrl } from 'features/Auth/constants'
@@ -16,20 +17,19 @@ export const LinkedInStep: FC<ILinkedInStep> = ({
 }) => {
   const handleSignUp = async () => {
     localStorage.setItem('onboardingProfile', JSON.stringify(onboardingProfile))
-    const deviceId = localStorage.getItem('deviceId')
-    if (deviceId) {
-      const fcmToken = await getTokenFcm()
-      const device = {
-        id: deviceId,
-        os: window.navigator.appVersion,
-        fcm_token: fcmToken,
-        voip_token: VOIP_TOKEN,
-        bundle: BUNDLE
-      }
-
-      await profileAPI.afterSignup({ ...onboardingProfile, device } as any)
-      window.open(linkedInAuthUrl, '_self')
+    const deviceId = localStorage.getItem('deviceId') || uuidv4()
+    localStorage.setItem('deviceId', deviceId)
+    const fcmToken = await getTokenFcm()
+    const device = {
+      id: deviceId,
+      os: window.navigator.appVersion,
+      fcm_token: fcmToken,
+      voip_token: VOIP_TOKEN,
+      bundle: BUNDLE
     }
+
+    await profileAPI.afterSignup({ ...onboardingProfile, device } as any)
+    window.open(linkedInAuthUrl, '_self')
   }
   return (
     <div>
